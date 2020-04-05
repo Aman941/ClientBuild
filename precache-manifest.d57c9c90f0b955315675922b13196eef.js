34faa3f509e4a8cb9bee6e2a3a137a4ef5993928
Aman941 <aman.verma9412345@gmail.com>
2020-04-05 16:11:27 +0530

    build

---
+++ precache-manifest.d57c9c90f0b955315675922b13196eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486fd624f1dba397f8121520f9ad1dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84a07ffd51a5e9031c5",
+    "url": "/static/js/2.46b77cfa.chunk.js"
+  },
+  {
+    "revision": "ec16aca364cff818a9c913df0570541f",
+    "url": "/static/js/2.46b77cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2812762a4115b744a0",
+    "url": "/static/js/main.1d39cdb2.chunk.js"
+  },
+  {
+    "revision": "d5a4674d5b6d3d6954a5",
+    "url": "/static/js/runtime-main.81fc0c82.js"
+  }
+]);
